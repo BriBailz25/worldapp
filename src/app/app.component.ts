2b751f9914972a13714855d2cffa7a0bc683b014
BriBailz25 <babz8216@gmail.com>
2026-08-28 14:33:44 -0500

    Part D

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'world-app';
+  name: string = 'Russia';
+  capital: string = 'Moscow';
+  region: string = 'Europe';
+  income: string = 'High income';
+  population: string = '146,000,000';
+  currency: string = 'Russian Ruble';
 }
+
